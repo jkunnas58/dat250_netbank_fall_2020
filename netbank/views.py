769f5cef4d36a0865b5845313a35ad39e96faabc
amikalsen <amikalsen@homtail.com>
2020-10-12 20:40:14 +0200

    Small update

--- netbank/views.py
+++ netbank/views.py
@@ -4,7 +4,6 @@ from netbank import app #, query_db
 
 @app.route('/')
 def index():
-    # articles = query_db('SELECT rowid, * FROM article;')
     return render_template('index.html')
 
 @app.route('/login_page')
@@ -14,10 +13,3 @@ def login_page():
 @app.route('/logged_in_page')
 def logged_in_page():
     return render_template('logged_in_page.html')
-
-
-# @app.route('/article/<int:article_id>')
-# def article(article_id):
-#     article = query_db(
-#         'SELECT * FROM article WHERE id=?;', (str(article_id)), True)
-#     return render_template('article.html', article=article)
