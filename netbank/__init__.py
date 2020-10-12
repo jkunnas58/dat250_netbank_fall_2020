ef62a91199248f27f5a243269dca1e99edc6d357
Jari <jarikunnas@hotmail.com>
2020-10-12 22:16:23 +0200

    sqlaclchemy database first commit

--- netbank/__init__.py
+++ netbank/__init__.py
@@ -1,6 +1,21 @@
 from flask import Flask, g
-# import sqlite3
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////database.db'
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    # email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
 
 # DATABASE = 'database.db'
 
