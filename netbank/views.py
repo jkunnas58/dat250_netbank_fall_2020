1fad874cc7e5e90a8f3509c85d9b32dfa7c6f1e6
amikalsen <amikalsen@homtail.com>
2020-10-12 03:17:39 +0200

    Worked on templates

--- netbank/views.py
+++ netbank/views.py
@@ -5,7 +5,15 @@ from netbank import app #, query_db
 @app.route('/')
 def index():
     # articles = query_db('SELECT rowid, * FROM article;')
-    return f"<h1>Hello world</h1>"
+    return render_template('index.html')
+
+@app.route('/login_page')
+def login_page():
+    return render_template('login_page.html')
+
+@app.route('/logged_in_page')
+def logged_in_page():
+    return render_template('logged_in_page.html')
 
 
 # @app.route('/article/<int:article_id>')
