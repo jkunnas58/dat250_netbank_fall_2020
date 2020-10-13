e535eb9c6346788091b1281e68583f8d99babf21
amikalsen <amikalsen@homtail.com>
2020-10-13 04:46:21 +0200

    Update views.py

--- netbank/views.py
+++ netbank/views.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
 from netbank import app #, query_db
 from forms import RegistrationForm, LoginForm
 
@@ -13,6 +13,8 @@ def index():
 def register():
     form = RegistrationForm()
     # if form.validate_on_submit()
+        # flash(f'Account created for {form.username.data}', 'success')
+        # return redirect(url_for('home'))
     return render_template('register.html', form=form)
 
 @app.route('/login_page.html')
