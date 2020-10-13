bcc11aad044cce2b66fe7335c5a0015012428e09
amikalsen <amikalsen@homtail.com>
2020-10-13 20:21:47 +0200

    Update views.py

--- netbank/views.py
+++ netbank/views.py
@@ -1,15 +1,28 @@
-from flask import render_template
+from flask import Flask, render_template, url_for, flash, redirect
 from netbank import app , db
 db.create_all()
+from forms import RegistrationForm, LoginForm
+
+
+app.config['SECRET KEY'] = '' #need to input secret key
 
 @app.route('/')
 @app.route('/index.html')
 def index():
     return render_template('index.html')
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    # if form.validate_on_submit()
+        # flash(f'Account created for {form.username.data}', 'success')
+        # return redirect(url_for('home'))
+    return render_template('register.html', form=form)
+
 @app.route('/login_page.html')
 def login_page():
-    return render_template('login_page.html')
+    form = LoginForm()
+    return render_template('login_page.html', form=form)
 
 @app.route('/logged_in_page.html')
 def logged_in_page():
