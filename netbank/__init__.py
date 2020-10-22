3d5dd92da9373c8b0a1c69d53e1fb56e0aad4d27
amikalsen <amikalsen@homtail.com>
2020-10-22 23:23:29 +0200

    update

--- netbank/__init__.py
+++ netbank/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 import os
-import psycopg2
+# import psycopg2
 
 
 app = Flask(__name__)
