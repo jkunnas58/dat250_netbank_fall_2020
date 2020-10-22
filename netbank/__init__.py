92b02a148ffc2182f2a331f166c549a690c9727d
Jari <jarikunnas@hotmail.com>
2020-10-22 22:55:56 +0200

    updated package for postgresql

--- netbank/__init__.py
+++ netbank/__init__.py
@@ -3,11 +3,16 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 import os
+import psycopg2
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '958efa56da92ed114e48d8b256eebeffdbcb24f30bf6d500'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 #for local database
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+
+#for local postgresql database
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
 #for Heroku postgresql deployment
